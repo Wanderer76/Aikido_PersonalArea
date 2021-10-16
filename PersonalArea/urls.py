484c466eeda6d0b4c1e9804aa0257628df83f57e
Wanderer76 <ateplinsky@mail.ru>
2021-10-16 16:43:44 +0500

    изменил способ задания id в Aikido_Member

--- PersonalArea/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from PersonalArea import views
-
-urlpatterns = [
-    path('students/', views.aikido_students_list),
-    path('students/<int:pk>/seminars',views.student_seminars)
-]
