ab5b9ff926dae3df7652a608a7dc974fa5f7f6f6
shiriaeva <Elena.Shiriaeva@urfu.me>
2021-10-25 20:56:23 +0500

    добавила парсер из excel в бд

--- Utilities/XlsParser.py
+++ Utilities/XlsParser.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 from ctypes import ArgumentError
+import os
 
 import openpyxl
 from openpyxl.styles import numbers, PatternFill
@@ -8,7 +9,49 @@ from PersonalArea import models
 
 
 def parseXlcToDb(xlcFile):
-    pass
+
+    book = openpyxl.open(xlcFile, read_only=True)
+    book.iso_dates = True
+    sheet = book.active
+
+    for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
+        seminar = models.Seminar()
+        seminar.name = os.path.basename(xlcFile)
+        seminar.region = int(row[6].value)
+        seminar.club = row[7].value
+        seminar.trainer = row[8].value
+        seminar.city = row[11].value
+        seminar.attestation_date = row[12].value
+        seminar.start_date = row[10].value
+        seminar.oldKu = int(row[9].value)
+        seminar.newKu = int(row[13].value)
+        seminar.isChild = True if row[14].value == '+' else False
+        seminar.examiner = row[15].value
+
+        member_id = row[0].value
+
+        if not (models.Aikido_Member.objects.filter(id=member_id).exists()):
+            aikiboy = models.Aikido_Member.objects.create(
+                id = member_id,
+                password = '',
+                surname = row[2].value,
+                name = row[3].value,
+                second_name = row[4].value,
+                birthdate = row[5].value,
+                region = int(row[6].value),
+                city = 'Екатеринбург',
+                club = row[7].value,
+                photo = None,
+                isTrainer = False,
+                trainer_id = row[1].value
+         )
+            seminar.member = aikiboy
+            seminar.save()
+
+        else:
+            aikiboy = models.Aikido_Member.objects.get(id = member_id)
+            seminar.member = aikiboy
+            seminar.save()
 
 
 def parseRecordsToXlc(eventName):
