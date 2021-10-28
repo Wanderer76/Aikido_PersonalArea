e6c0a401b0bbc168f0aec5c1d64aa9da3ace6e90
Wanderer76 <ateplinsky@mail.ru>
2021-10-28 20:14:46 +0500

    Функции работы с бд используют транзакции

--- Utilities/XlsParser.py
+++ Utilities/XlsParser.py
@@ -2,6 +2,8 @@ import datetime
 import os
 import openpyxl
 from ctypes import ArgumentError
+
+from django.db import transaction
 from openpyxl.styles import numbers, PatternFill
 from PersonalArea import models
 from Utilities import password_generantor
@@ -25,17 +27,20 @@ def get_ku(record):
             pass
         else:
             return int(record.replace(' ', '').replace("кю", ""))
+    raise ArgumentError('Таблица заполнена не правильно')
 
 
 def parse_eu_date_to_us(record):
     return datetime.datetime.strptime(record, "%d.%m.%Y").strftime("%Y-%m-%d")
 
 
+@transaction.atomic
 def parseXlcToDb(xlcFile):
     book = openpyxl.open(xlcFile, read_only=True)
     book.iso_dates = True
     sheet = book.active
     print(sheet.max_row)
+
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
         fileName = os.path.basename(xlcFile)
         seminar = models.Seminar()
@@ -56,7 +61,7 @@ def parseXlcToDb(xlcFile):
         if not (models.Aikido_Member.objects.filter(id=member_id).exists()):
             aikiboy = models.Aikido_Member.objects.create(
                 id=member_id,
-                password= password_generantor.generate_password(),
+                password=password_generantor.generate_password(),
                 name=row[1].value,
                 surname=row[0].value,
                 second_name=row[2].value,
@@ -83,6 +88,7 @@ def set_trainer_status(trainer_id):
         trainer.save()
 
 
+@transaction.atomic
 def parseRecordsToXlc(eventName):
     wb = openpyxl.Workbook(iso_dates=True)
     workSheet = wb.active
