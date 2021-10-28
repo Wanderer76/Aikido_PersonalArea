f1b1d6afd70acbdfccf500cae41f558db633c514
Wanderer76 <ateplinsky@mail.ru>
2021-10-28 15:27:41 +0500

    Изменил индексы в соответствии с таблицей, вынес блок формирования строки exel в отдельный метод

--- Utilities/XlsParser.py
+++ Utilities/XlsParser.py
@@ -1,15 +1,11 @@
-import datetime
-import json
-from ctypes import ArgumentError
 import os
-
 import openpyxl
+from ctypes import ArgumentError
 from openpyxl.styles import numbers, PatternFill
 from PersonalArea import models
 
 
 def parseXlcToDb(xlcFile):
-
     book = openpyxl.open(xlcFile, read_only=True)
     book.iso_dates = True
     sheet = book.active
@@ -17,13 +13,12 @@ def parseXlcToDb(xlcFile):
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
         seminar = models.Seminar()
         seminar.name = os.path.basename(xlcFile)
-        seminar.region = int(row[6].value)
         seminar.club = row[7].value
         seminar.trainer = row[8].value
         seminar.city = row[11].value
-        seminar.attestation_date = row[12].value
         seminar.start_date = row[10].value
-        seminar.oldKu = int(row[9].value)
+        seminar.attestation_date = row[12].value
+        seminar.oldKu = int(row[3].value)
         seminar.newKu = int(row[13].value)
         seminar.isChild = True if row[14].value == '+' else False
         seminar.examiner = row[15].value
@@ -32,30 +27,28 @@ def parseXlcToDb(xlcFile):
 
         if not (models.Aikido_Member.objects.filter(id=member_id).exists()):
             aikiboy = models.Aikido_Member.objects.create(
-                id = member_id,
-                password = '',
-                surname = row[2].value,
-                name = row[3].value,
-                second_name = row[4].value,
-                birthdate = row[5].value,
-                region = int(row[6].value),
-                city = 'Екатеринбург',
-                club = row[7].value,
-                photo = None,
-                isTrainer = False,
-                trainer_id = row[1].value
-         )
+                id=member_id,
+                password='',
+                name=row[1].value,
+                surname=row[0].value,
+                second_name=row[2].value,
+                birthdate=row[5].value,
+                region=int(row[6].value),
+                club=row[7].value,
+                photo=None,
+                isTrainer=False,
+                trainer_id=row[9].value
+            )
             seminar.member = aikiboy
             seminar.save()
 
         else:
-            aikiboy = models.Aikido_Member.objects.get(id = member_id)
+            aikiboy = models.Aikido_Member.objects.get(id=member_id)
             seminar.member = aikiboy
             seminar.save()
 
 
 def parseRecordsToXlc(eventName):
-    # нужны датарождения, фио
     wb = openpyxl.Workbook(iso_dates=True)
     workSheet = wb.active
     workSheet.append(["Фамилия", "Имя", "Отчество", "степень кю/дан", "#ID", "Дата рождения",
@@ -70,15 +63,7 @@ def parseRecordsToXlc(eventName):
     for seminar in seminars.values():
         try:
             member = models.Aikido_Member.objects.get(id=seminar["member_id"])
-            workSheet.append([
-                member.surname, member.name, member.second_name, str(seminar['oldKu']) + "кю", member.id,
-                member.birthdate,
-                seminar['region'],
-                seminar['club'], seminar['trainer'], member.trainer_id, seminar['start_date'], seminar['city'],
-                seminar['attestation_date'],
-                                                                 str(seminar['newKu']) + "кю",
-                "да" if seminar['isChild'] else "нет", seminar['examiner']])
-
+            workSheet.append(form_row(member, seminar))
         except:
             continue
     for i in workSheet.iter_rows(min_row=1, max_row=workSheet.max_row):
@@ -86,6 +71,17 @@ def parseRecordsToXlc(eventName):
         i[10].number_format = numbers.FORMAT_DATE_DDMMYY
         i[12].number_format = numbers.FORMAT_DATE_DDMMYY
     wb.save(f"{eventName}.xlsx")
+
+
+def form_row(member, seminar):
+    return [
+        member.surname, member.name, member.second_name, str(seminar['oldKu']) + "кю", member.id,
+        member.birthdate,
+        member.region,
+        seminar['club'], seminar['trainer'], member.trainer_id, seminar['start_date'], seminar['city'],
+        seminar['attestation_date'],
+                                                         str(seminar['newKu']) + "кю",
+        "да" if seminar['isChild'] else "нет", seminar['examiner']]
 
 
 def set_blue_row(workSheet):
