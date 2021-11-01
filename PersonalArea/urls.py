512cfcabe63d425f439793bf45aedb5fdda72034
Wanderer76 <ateplinsky@mail.ru>
2021-11-01 13:58:51 +0500

    Создано api, для доступа к созданию событий и запросов

--- PersonalArea/urls.py
+++ PersonalArea/urls.py
@@ -5,4 +5,6 @@ from PersonalArea import views
 urlpatterns = [
     path('students/', views.aikido_students_list),
     path('students/<int:pk>/seminars/', views.student_seminars),
+    path('api/v1/events/create/', views.create_event),
+    path('api/v1/events/makerequest/', views.make_request),
 ]
