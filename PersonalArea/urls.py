535ad9f9f5e51b7dc6ac6655bc7b773f4c746c75
shiriaeva <Elena.Shiriaeva@urfu.me>
2021-11-11 16:56:32 +0500

    создание унифицированного указателя ресурсов

--- PersonalArea/urls.py
+++ PersonalArea/urls.py
@@ -5,9 +5,9 @@ from PersonalArea import views
 
 urlpatterns = [
     path('students/', views.aikido_students_list),
-    path('students/<int:pk>/seminars/', views.student_seminars),
     path('api/v1/account/login/', csrf_exempt(views.LoginAPIView.as_view())),
     path('api/v1/seminar/<str:seminar_name>/', views.DownloadRequests.as_view()),
     path('api/v1/events/create/', csrf_exempt(views.CreateEvent.as_view())),
     path('api/v1/events/makerequest/', csrf_exempt(views.CreateRequest.as_view())),
+    path('api/v1/account/profile/', csrf_exempt(views.StudentInfo.as_view())),
 ]
