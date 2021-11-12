98ab7035e7207ebd45e5d80f428802b0ee0ad2d6
MrNickev <nikitaz2003@yandex.ru>
2021-11-12 21:52:12 +0500

    добавил всплывающую надпись при неверном пароле

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -1,6 +1,9 @@
 const url = "http://localhost:8000/api/v1/account/login/";
 
 let butt = document.querySelector(".enter-button");
+let errorMessage = document.querySelector('.error-message');
+let idForm = document.getElementById('id');
+let passForm = document.getElementById('pass');
 let id;
 let pass;
 let xhr = new XMLHttpRequest();
@@ -8,12 +11,15 @@ xhr.responseType = 'json';
 let token;
 
 butt.onclick = function () {
-    id = document.getElementById('id').value;
-    pass = document.getElementById('pass').value;
+    id = idForm.value;
+    pass = passForm.value;
 
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             token = xhr.response;
+            if (checkToken(token)){
+                errorMessage.classList.remove('hide');
+            }
         }
         console.log(xhr.response)
     };
@@ -26,4 +32,16 @@ butt.onclick = function () {
     console.log(json);
     xhr.open("POST", url, true);
     xhr.send(json);
+}
+
+idForm.onchange = function () {
+    errorMessage.classList.add('hide');
+}
+
+passForm.onchange = function () {
+    errorMessage.classList.add('hide')
+}
+
+function checkToken(token) {
+    return token.includes("error");
 }
