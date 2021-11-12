d2329d9c9a3f2074abd9e632a5a386d417b299b6
MrNickev <nikitaz2003@yandex.ru>
2021-11-12 12:21:22 +0500

    реализована отправка данных по форме

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -1,22 +1,33 @@
 const url = "http://localhost:8000/api/v1/account/login/";
 
+// let loginInput = document.querySelector("#id");
+// let passInput = document.querySelector("#pass");
+let butt = document.querySelector(".enter-button");
 let id = 1345;
 let pass = "qwerty1234";
 
-let xhr = new XMLHttpRequest();
-xhr.responseType = 'json';
+butt.onclick = function () {
+    id = document.getElementById('id').value;
+    pass = document.getElementById('pass').value;
 
-let json = {"id": id,
-    "password": pass};
+    let xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
-let token = "";
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        token = xhr.response.message;
-    }
-};
-xhr.open("POST", url, true);
-xhr.send(json);
+    let json = {"id": id,
+        "password": pass};
+
+    let token = "";
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            token = xhr.response.message;
+        }
+    };
+    console.log(json);
+    xhr.open("POST", url, true);
+    xhr.send(json);
+
+}
+
 
 
 
