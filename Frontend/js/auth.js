b7b774c9254be3345c9ea66e407fe35429590ae7
Wanderer76 <ateplinsky@mail.ru>
2021-11-12 13:29:16 +0500

    Update auth.js

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -1,33 +1,27 @@
 const url = "http://localhost:8000/api/v1/account/login/";
 
-// let loginInput = document.querySelector("#id");
-// let passInput = document.querySelector("#pass");
 let butt = document.querySelector(".enter-button");
-let id = 1345;
-let pass = "qwerty1234";
+let id;
+let pass;
+let xhr = new XMLHttpRequest();
+xhr.responseType = 'json';
+let token;
 
 butt.onclick = function () {
     id = document.getElementById('id').value;
     pass = document.getElementById('pass').value;
 
-    let xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    let json = {"id": id,
-        "password": pass};
-
-    let token = "";
     xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+        if (this.readyState == 4 && this.status == 200 ) {
             token = xhr.response.message;
         }
+        console.log(xhr.response)
     };
+
+    let json = JSON.stringify({"id": parseInt(id),
+        "password": pass.toString()});
+
     console.log(json);
     xhr.open("POST", url, true);
     xhr.send(json);
-
 }
-
-
-
-
