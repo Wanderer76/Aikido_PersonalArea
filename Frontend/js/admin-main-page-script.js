d1c5748efbac6fa87f466a59558a0d131dfef99d
MrNickev <nikitaz2003@yandex.ru>
2021-11-23 17:07:44 +0500

    Добавление отклика кнопкам таблицы в admin-main-page

--- Frontend/js/admin-main-page-script.js
+++ Frontend/js/admin-main-page-script.js
@@ -2,11 +2,13 @@ let url = "http://localhost:8000/api/v1/admin/seminar_statistic/";
 let xhr = new XMLHttpRequest();
 let storage = window.sessionStorage;
 let data;
-
 let comingActivitiesHeader = document.getElementById('coming-activities-header');
 let previewActivitiesHeader = document.getElementById('preview-activities-header');
+let received = false;
 
-console.log(storage.getItem('user_token'));
+xhr.open('GET', url);
+xhr.setRequestHeader('Authorization', 'Token ' + storage.getItem('user_token'))
+xhr.send();
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -14,13 +16,18 @@ xhr.onreadystatechange = function() {
         console.log(data);
         addDataToTable(data, comingActivitiesHeader, 'upcoming');
         addDataToTable(data, previewActivitiesHeader, 'past');
+        buttonsFunc();
     }
 }
 
-xhr.open('GET', url);
-xhr.setRequestHeader('Authorization', 'Token ' + storage.getItem('user_token'))
-xhr.send();
-
+function buttonsFunc() {
+    let buttons = document.querySelectorAll('.more_details');
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = function () {
+            console.log(i)
+        }
+    }
+}
 
 function addDataToTable(data, tagAfterInserting, tag) {
     let activities = JSON.parse(data);
@@ -40,6 +47,7 @@ function addDataToTable(data, tagAfterInserting, tag) {
         output.classList.add('row');
         output.classList.add('align-items-center');
         output.innerHTML =
+            '   <div class="col" id="name'+ i +'">' + activity.event_name + '</div>' +
             '   <div class="col date">\n' +
             '       '+ createDateOutput(activity.date_of_event, activity.end_of_event) + '\n' +
             '   </div>\n' +
@@ -49,7 +57,7 @@ function addDataToTable(data, tagAfterInserting, tag) {
             '   <div class="col">\n' + activity.responsible_club +
             '   </div>\n' +
             '   <div class="col">\n' +
-            '       <button type="button" name="more_details" class="more_details">Подробнее</button>\n' +
+            '       <button type="button" name="more_details" class="more_details" id="more-button-'+i+'">Подробнее</button>\n' +
             '   </div>\n';
 
         tagAfterInserting.parentElement.insertBefore(output, tagAfterInserting.nextSibling);
