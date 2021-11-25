27b44fc6d3c6950ec48dee68e80a491d29e798bd
Wanderer76 <ateplinsky@mail.ru>
2021-11-25 21:56:34 +0500

    Добавлены url загрузки и выгрузки файлов с мероприятия

--- PersonalArea/urls.py
+++ PersonalArea/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('students/', views.aikido_students_list),
     path('api/v1/account/login/', csrf_exempt(views.LoginAPIView.as_view())),
     path('api/v1/account/admin/login/', csrf_exempt(views.AdminLoginAPIView.as_view())),
-    path('api/v1/seminar/<str:seminar_name>/', views.DownloadRequests.as_view()),
     path('api/v1/events/create/', csrf_exempt(views.CreateEvent.as_view())),
     path('api/v1/events/requests/create/', csrf_exempt(views.CreateRequest.as_view())),
     path('api/v1/events/requests/<str:event_name>/', views.TrainerEventRequest.as_view()),
@@ -16,4 +15,9 @@ urlpatterns = [
     path('api/v1/admin/seminar_statistic/', views.EventsList.as_view()),
     path('api/v1/admin/event_statistic/<str:event_name>/', views.EventView.as_view()),
     path('api/v1/admin/seminar/<str:event_name>/', views.SeminarStatistic.as_view()),
+    path('api/v1/admin/seminar/download/<str:seminar_name>/', views.DownloadRequests.as_view()),
+    path('api/v1/admin/seminar/load/<str:filename>/', csrf_exempt(views.LoadRequests.as_view())),
+
 ]
+
+
