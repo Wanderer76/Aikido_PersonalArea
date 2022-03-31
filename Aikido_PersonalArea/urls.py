a59d2a05a226109ee96f67b1d02412e42f8a4320
Wanderer76 <53403320+Wanderer76@users.noreply.github.com>
2022-03-31 22:06:17 +0500

    Добавил поле slug в модель Club

--- Aikido_PersonalArea/urls.py
+++ Aikido_PersonalArea/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('PersonalArea.urls')),
+    path('clubs/',include('clubs.urls'))
 ]
--- clubs/models.py
+++ clubs/models.py
@@ -5,6 +5,7 @@ class Club(models.Model):
     name = models.CharField(name="name", max_length=50, primary_key=True)
     city = models.CharField(name="city", max_length=50)
     area = models.CharField(name="area", max_length=50)
-    main_trainers = models.ForeignKey(related_name="main_trainers", to="PersonalArea.Aikido_Member",
+    main_trainers = models.ForeignKey(related_name="main_trainers", to="PersonalArea.Aikido_Member", to_field='id',
                                       on_delete=models.CASCADE, null=True)
     logo = models.ImageField(name="logo", upload_to='logo_images/')
+    slug = models.SlugField(name='slug', max_length=50, unique=True, null=False)
