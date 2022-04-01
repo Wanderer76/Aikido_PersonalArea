842c1eb50f9370e4d479cfeb9a489fb1531fc3d3
Wanderer76 <53403320+Wanderer76@users.noreply.github.com>
2022-04-01 21:48:08 +0500

    Адаптировал под новые модели. Исправил баг с удалением людей

--- Utilities/services.py
+++ Utilities/services.py
@@ -1,10 +1,17 @@
 import datetime
+import os
+import tempfile
+from typing import List, Dict, Union, Optional, BinaryIO
+
+from openpyxl import Workbook
 from openpyxl.styles import PatternFill, numbers
+from openpyxl.worksheet.worksheet import Worksheet
+
 from PersonalArea import models
 from ctypes import ArgumentError
 
 
-def parse_id_from_xls(record):
+def parse_id_from_xls(record: Union[Union[int, str], None]) -> int:
     result = None
     if record is None:
         result = generate_id()
@@ -22,12 +29,14 @@ def parse_id_from_xls(record):
         return result
 
 
-def get_ku(record):
+def get_ku(record: Union[int, Union[str, None]]) -> Optional[int]:
     if isinstance(record, int):
         return record
     else:
         if record == "" or record is None:
             return None
+        if 'детский' in record:
+            record = record.replace('детский', '')
         if "дан" in record:
             ku = record.replace('дан', '').replace(' ', '')
             if isinstance(ku, int):
@@ -40,26 +49,30 @@ def get_ku(record):
             raise ArgumentError(f'Не правильно заполнена ячейка {record}')
 
 
-def parse_eu_date_to_us(record):
+def parse_eu_date_to_us(record: str) -> str:
     if len(record.split(' ')) > 1:
         return record.split(' ')[0]
     return datetime.datetime.strptime(record, "%d.%m.%Y").strftime("%Y-%m-%d")
 
 
-def set_trainer_status(trainer_id):
+def set_trainer_status(trainer_id: int) -> None:
     if models.Aikido_Member.objects.filter(id=trainer_id).exists():
         trainer = models.Aikido_Member.objects.get(id=trainer_id)
         trainer.isTrainer = True
         trainer.save()
 
 
-def create_row(request, event, member=None):
+def create_row(request: Dict[str, str], event: models.Events, member=None) -> List[str]:
     trainer = models.Aikido_Member.objects.get(id=request['trainer_id'])
-    oldKu = 0
+    last = 0
+    ku_suffix = ''
     if member is not None:
-        lastSeminar = models.Seminar.objects.filter(member=member).order_by('attestation_date').values_list('newKu')
-        if lastSeminar.exists():
-            oldKu = lastSeminar[0][0]
+        member_achievements = models.Achievements.objects.filter(member=member, received_ku__isnull=False).order_by(
+            'attestation_date')
+        if member_achievements.exists():
+            last = member_achievements.values_list('received_ku')[member_achievements.count() - 1][0]
+            if member_achievements[member_achievements.count() - 1].is_child:
+                ku_suffix = ' детский'
     second_name = ''
     if request['second_name'] != 'None' and request['second_name'] != '':
         second_name = request['second_name']
@@ -69,7 +82,7 @@ def create_row(request, event, member=None):
 
     member_id = member.id if member is not None else ''
     return [
-        request['surname'], request['name'], second_name, parse_ku(oldKu), member_id,
+        request['surname'], request['name'], second_name, parse_ku(last) + ku_suffix, member_id,
         request['birthdate'],
         trainer.region,
         trainer.club.name,
@@ -79,46 +92,44 @@ def create_row(request, event, member=None):
         event.date_of_event, '', '', event.responsible_trainer]
 
 
-def set_blue_row(workSheet):
-    for row in workSheet.iter_rows(min_row=1, max_row=1):
+def set_blue_row(work_sheet: Worksheet) -> None:
+    for row in work_sheet.iter_rows(min_row=1, max_row=1):
         for cell in row:
             cell.fill = PatternFill(patternType='solid', start_color='538dd5', end_color='538dd5')
 
 
-def generate_id():
+def generate_id() -> int:
     ids = sorted(models.Aikido_Member.objects.values_list("id", flat=True))
     limit = ids[-1]
-    missingNumbers = list(set(range(1, limit + 3)) - set(ids))
-    return missingNumbers[0]
+    missing_numbers = list(set(range(1, limit + 3)) - set(ids))
+    return missing_numbers[0]
 
 
-def check_seminars_for_exists(event_name):
-    seminars = models.Seminar.objects.filter(name=event_name).prefetch_related("member")
+def delete_achievement_if_exists(event_name: str) -> None:
+    achievements = models.Achievements.objects.filter(event_name=event_name) \
+        .prefetch_related("member")
+    if achievements.exists():
+        hasbiks_id = achievements.values_list("member", flat=True)
 
-    if seminars.exists():
-        hasbiks_id = seminars.values_list("member", flat=True)
         for i in hasbiks_id:
-            if seminars.filter(member_id=i).count() == 1:
-                seminars.get(member_id=i).delete()
-                models.Aikido_Member.objects.get(id=i).delete()
-            else:
-                seminars.get(member_id=i).delete()
+            if models.Achievements.objects.filter(member_id=i).count() == 1:
+                if achievements.filter(member_id=i).exists():
+                    models.Aikido_Member.objects.get(id=i).delete()
+            achievements.get(member_id=i).delete()
 
 
-def check_event_for_exists(event_name):
-    if models.Events.objects.filter(event_name=event_name).exists():
-        return True
-    return False
+def find_event_by_slug(event_slug: str) -> Union[models.Events, None]:
+    return models.Events.objects.filter(slug=event_slug).first()
 
 
-def set_date_format(workSheet):
-    for i in workSheet.iter_rows(min_row=1, max_row=workSheet.max_row):
+def set_date_format(work_sheet: Worksheet) -> None:
+    for i in work_sheet.iter_rows(min_row=1, max_row=work_sheet.max_row):
         i[5].number_format = numbers.FORMAT_DATE_DDMMYY
         i[10].number_format = numbers.FORMAT_DATE_DDMMYY
         i[12].number_format = numbers.FORMAT_DATE_DDMMYY
 
 
-def get_day_before(date_of_event: datetime.date):
+def get_day_before(date_of_event: datetime.date) -> datetime.datetime:
     day_before = date_of_event - datetime.timedelta(1)
     return datetime.datetime(day_before.year, day_before.month, day_before.day, 23, 59, 59, 0)
 
@@ -129,3 +140,19 @@ def parse_ku(value: int) -> str:
     if value == 0:
         return ""
     return f"{value} кю"
+
+
+def get_xlsx_file_obj(work_book: Workbook, event_slug: str) -> BinaryIO:
+    temp_file = tempfile.NamedTemporaryFile()
+    temp_file.name = event_slug + ".xlsx"
+    work_book.save(temp_file.name)
+    work_book.close()
+    stream_file = open(temp_file.name, 'rb')
+    original_close = stream_file.close
+
+    def orig_cl():
+        original_close()
+        os.remove(temp_file.name)
+
+    stream_file.close = orig_cl
+    return stream_file
