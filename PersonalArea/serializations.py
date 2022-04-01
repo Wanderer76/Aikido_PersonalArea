f5003616c0fa130cd2cc4b56b7df92659c9b6fb7
Wanderer76 <53403320+Wanderer76@users.noreply.github.com>
2022-04-01 12:28:06 +0500

    Адаптировал Events_Serializer под новую модель

--- PersonalArea/serializations.py
+++ PersonalArea/serializations.py
@@ -1,6 +1,13 @@
+import datetime
+
+from pytils.translit import slugify
+
 from PersonalArea.models import Aikido_Member, Seminar, Request, Events
 from rest_framework import serializers
 from django.contrib.auth import authenticate
+
+from Utilities.services import get_day_before
+from clubs.models import Club
 
 
 class Aikido_MemberSerializer(serializers.ModelSerializer):
@@ -48,7 +55,30 @@ class Requests_Serializer(serializers.ModelSerializer):
         model = Request
         exclude = ['id']
 
+
 class Events_Serializer(serializers.ModelSerializer):
+    start_record_date = datetime.datetime.today()
+    end_record_date = datetime.datetime.today()
+    slug = serializers.SlugField(default="")
+    ''' def validate(self, attrs):
+        print(attrs)
+
+    def create(self, validated_data):
+        event = Events()
+        event.event_name = validated_data['event_name']
+        event.date_of_event = validated_data['date_of_event']
+        event.end_of_event = validated_data['end_of_event']
+        event.start_record_date = datetime.datetime.today()
+        event.end_record_date = get_day_before(datetime.date.fromisoformat(validated_data['date_of_event']))
+        event.city = validated_data['city']
+        event.responsible_club = Club.objects.get(name=validated_data['responsible_club'])
+        event.slug = slugify(validated_data['event_name'])
+        event.schedule = validated_data['schedule']
+        event.contacts = validated_data['contacts']
+        event.poster = validated_data['poster']
+        return event
+'''
+
     class Meta:
         model = Events
         fields = '__all__'
