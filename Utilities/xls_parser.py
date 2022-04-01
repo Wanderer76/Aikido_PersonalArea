1a45576d3b9a13e2360a3cd17ecd46bddc177996
Wanderer76 <53403320+Wanderer76@users.noreply.github.com>
2022-04-01 21:47:27 +0500

    Адаптировал функции под обновленные модели. Исправил баг с созданием файла xlsx

--- Utilities/xls_parser.py
+++ Utilities/xls_parser.py
@@ -1,8 +1,10 @@
-import tempfile
 from ctypes import ArgumentError
+from typing import BinaryIO
+
 import openpyxl
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.db import transaction
+
 import PersonalArea.serializations
 from PersonalArea import models
 from Utilities import password_generantor
@@ -10,31 +12,28 @@ from Utilities import services
 
 
 @transaction.atomic
-def parseXlsToDb(seminar_name: str, xlsxFile: InMemoryUploadedFile) -> str:
-    book = openpyxl.load_workbook(xlsxFile)
+def parseXlsToDb(event_slug: str, xlsx_file: InMemoryUploadedFile) -> str:
+    book = openpyxl.load_workbook(xlsx_file)
     book.iso_dates = True
     sheet = book.active
-    event_name = seminar_name
-    if not services.check_event_for_exists(event_name):
+    event = services.find_event_by_slug(event_slug)
+
+    if event is None:
         return 'Мероприятие не существует'
 
-    services.check_seminars_for_exists(event_name)
+    services.delete_achievement_if_exists(event.event_name)
 
     for row in sheet.iter_rows(min_row=2, max_row=sheet.max_row):
         member_id = services.parse_id_from_xls(row[4].value)
-        if models.Seminar.objects.filter(name=event_name, member__id=member_id).exists():
+        if models.Achievements.objects.filter(event_name=event.event_name, member__id=member_id).exists():
             raise ArgumentError(f"Ячейка заполнена не правильно {row[4]}")
-        seminar = models.Seminar()
-        seminar.name = event_name
-        seminar.club = row[7].value
-        seminar.trainer = row[8].value
-        seminar.city = row[11].value
-        seminar.start_date = row[10].value
-        seminar.attestation_date = row[12].value
-        seminar.oldKu = services.get_ku(row[3].value)
-        seminar.newKu = services.get_ku(row[13].value)
-        seminar.isChild = True if row[14].value == '+' else False
-        seminar.examiner = row[15].value
+
+        achievement = models.Achievements()
+        achievement.event_name = event.event_name
+        achievement.attestation_date = row[12].value
+        achievement.received_ku = services.get_ku(row[13].value)
+        achievement.is_child = True if row[14].value == '+' else False
+
         trainer_id = services.parse_id_from_xls(row[9].value)
         services.set_trainer_status(trainer_id)
         if not (models.Aikido_Member.objects.filter(id=member_id).exists()):
@@ -54,42 +53,39 @@ def parseXlsToDb(seminar_name: str, xlsxFile: InMemoryUploadedFile) -> str:
             serializer = PersonalArea.serializations.RegistrationSerializer(data=data)
             if serializer.is_valid():
                 aikiboy = serializer.save()
-                seminar.member = aikiboy
-                seminar.save()
+                achievement.member = aikiboy
+                achievement.save()
             else:
                 raise ArgumentError(f"Ошибка в строке {row}")
         else:
             aikiboy = models.Aikido_Member.objects.get(id=member_id)
-            seminar.member = aikiboy
-            seminar.save()
+            achievement.member = aikiboy
+            achievement.save()
 
     return 'Ведомость загружена'
 
 
 @transaction.atomic
-def createXlsxFromRequests(eventName: str) -> str:
+def createXlsxFromRequests(event_slug: str) -> BinaryIO:
     wb = openpyxl.Workbook(iso_dates=True)
-    workSheet = wb.active
-    workSheet.append(['Фамилия', 'Имя', 'Отчество', 'степень кю/дан', '#ID', 'Дата рождения',
-                      'Регион', 'Клуб', 'Тренер', 'id тренера', 'семинар', 'место проведения', 'аттестация',
-                      'Присвоенная степень', 'детский', 'Экзаменатор'])
-    services.set_blue_row(workSheet)
-    requests = models.Request.objects.filter(event_name=eventName)
+    work_sheet = wb.active
+    work_sheet.append(['Фамилия', 'Имя', 'Отчество', 'степень кю/дан', '#ID', 'Дата рождения',
+                       'Регион', 'Клуб', 'Тренер', 'id тренера', 'семинар', 'место проведения', 'аттестация',
+                       'Присвоенная степень', 'детский', 'Экзаменатор'])
+
+    services.set_blue_row(work_sheet)
+    event = models.Events.objects.get(slug=event_slug)
+
+    requests = models.Request.objects.filter(event_name=event.event_name)
     if not requests.exists():
         raise ArgumentError('Заявок на мероприятие нет')
-
-    event = models.Events.objects.get(event_name=eventName)
 
     for request in requests.values():
         if request['member_id'] is not None:
             member = models.Aikido_Member.objects.get(id=request['member_id'])
-            workSheet.append(services.create_row(request, event, member))
+            work_sheet.append(services.create_row(request, event, member))
         else:
-            workSheet.append(services.create_row(request, event))
+            work_sheet.append(services.create_row(request, event))
 
-    services.set_date_format(workSheet)
-    temp_file = tempfile.NamedTemporaryFile()
-    temp_file.name = f"{eventName}.xlsx"
-    wb.save(temp_file.name)
-    wb.close()
-    return temp_file.name
+    services.set_date_format(work_sheet)
+    return services.get_xlsx_file_obj(wb, event_slug)
