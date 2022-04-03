4463bed57c35dcce3e2b4c11f745c71828e061d5
shiriaeva <Elena.Shiriaeva@urfu.me>
2022-04-03 18:09:13 +0500

    добавлены урлы для клубов

--- clubs/urls.py
+++ clubs/urls.py
@@ -1,2 +1,13 @@
+from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+from clubs import views
+
 urlpatterns = [
+    path('api/v1/create_club/', csrf_exempt(views.CreateClub.as_view())),
+    path('api/v1/update_club/<str:slug>/', csrf_exempt(views.UpdateClub.as_view())),
+    path('api/v1/get_club/<str:slug>/', csrf_exempt(views.GetClub.as_view())),
+    path('api/v1/get_clubs/', csrf_exempt(views.GetClubs.as_view())),
+    path('api/v1/delete_club/<str:slug>/', csrf_exempt(views.DeleteClub.as_view())),
+
 ]
